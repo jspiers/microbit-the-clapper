62e87fa9cf388c34b88fd0f72e699c3f890858b9
Jeff <jeff@cusk.io>
2021-12-16 11:53:53 -0700

    Forever loop for isOn state and "hello" sound instead of text on boot

--- main.ts
+++ main.ts
@@ -2,30 +2,32 @@ input.onSound(DetectedSound.Quiet, function () {
     clapCount += 10
     if (clapCount > 10) {
         isOn = !(isOn)
-        if (isOn) {
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                . # . # .
-                . # # # .
-                . . . . .
-                `)
-        } else {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                . . # . .
-                . . # . .
-                . . # . .
-                `)
-        }
         clapCount = 0
     }
 })
 let isOn = false
 let clapCount: number
 clapCount = 0
-basic.showString("Clap on, clap off... the clapper!")
+soundExpression.hello.play()
+basic.forever(function () {
+    if (isOn) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+})
 loops.everyInterval(100, function () {
     if (clapCount > 0) {
         clapCount += -1
